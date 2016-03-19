5df20dab2dcdfef17a9860e0a9da11d6fa431f7e
KristianMH <krillemh@gmail.com>
2016-03-19 13:59:36 +0100

    g5 rapport og g4 done? only needs testing

--- g4/kudos/kudos/proc/memlimit.c
+++ g4/kudos/kudos/proc/memlimit.c
@@ -16,7 +16,7 @@ void *memlimit(void* new_end) {
   }
   pagetable_t *table = thread_get_current_thread_entry()->pagetable;
   int diff = (int)new_end - (int)old_end;
-  int pages = diff/PAGE_SIZE;
+  //int pages = diff/PAGE_SIZE;
   int left_on_page = 4096 - (int)old_end % PAGE_SIZE;
  
   // attempt to reduce internal fragementaion.
@@ -24,13 +24,17 @@ void *memlimit(void* new_end) {
     process_get_current_process_entry()->heap_end = new_end;
     return new_end;
   }
-  for (int i = 0; i < pages; i++) {
-    int vaddr = (int)old_end + PAGE_SIZE*(i+1);
+  //maps pages until the different is less the PAGE_SIZE
+  int vaddr = (int)old_end;
+  while(diff > left_on_page) {
+    vaddr += left_on_page;
     phys_page = physmem_allocblock();
-    memoryset((void*)ADDR_PHYS_TO_KERNEL(phys_page), 0, PAGE_SIZE);
-    vm_map(table, phys_page, vaddr, 1);    
+    memoryset((void*)ADDR_PHYS_TO_KERNEL(phys_page), 0, left_on_page);
+    vm_map(table, phys_page, vaddr, 1); 
+    diff -= left_on_page;
+    // start of new page.
+    left_on_page = PAGE_SIZE;
   }
-
   process_get_current_process_entry()->heap_end = new_end;
   return new_end;  
 }
